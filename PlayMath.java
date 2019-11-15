d09bbdccd5f0065b0d3cc477e854b259367c8d25
theodora-8 <57745592+theodora-8@users.noreply.github.com>
2019-11-15 20:24:35 +0700

    Add files via upload

---
+++ PlayMath.java
@@ -0,0 +1,10 @@
+// main class
+package mathgame1;
+
+public class PlayMath {
+
+    public static void main(String[] args) {
+        MathPrograms p = new MathPrograms();
+        p.Playthegame();
+    }
+}
