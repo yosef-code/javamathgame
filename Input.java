56f29d3d89bb089412791c0832dc7d558a71379b
yosef-code <57625810+yosef-code@users.noreply.github.com>
2019-11-15 14:14:19 +0700

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,11 @@
+
+package mathgame1;
+import java.util.Scanner;
+    public class Input {
+        public static void main (String[] args){
+            Scanner input = new Scanner(System.in);
+            String nama;
+            System.out.println ("Masukkan nama Anda : ");
+            nama=input.nextLine();
+        }}
+
